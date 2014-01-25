215f4fda21310c15d5822bb1ba4a8b24acd5eb26
Sean <zibai.wang@mail.utoronto.ca>
2014-01-25 15:55:45 -0500

    added picture feature

--- ECEA2/src/com/example/ecea2/DatabaseActivity.java
+++ ECEA2/src/com/example/ecea2/DatabaseActivity.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DatabaseActivity extends ListActivity {
   public void onCreate(Bundle icicle) {
@@ -25,5 +28,18 @@ public class DatabaseActivity extends ListActivity {
     setListAdapter(adapter);
   }
 
-} 
+   }
+  
+  /*  TO DO from id find the url to the picture
+  @Override
+  protected void onListItemClick(ListView l, View v, int position, long id) {
+    String item = (String) getListAdapter().getItem(position);
+    //Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    Intent intent = new Intent(this, ShowfileActivity.class);
+    Bundle extras = new Bundle();
+	extras.putString("EXTRA_FILENAME",item);
+	intent.putExtras(extras);
+	startActivity(intent);
+  }
+  */
 }
