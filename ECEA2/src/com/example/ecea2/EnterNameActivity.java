f8bb1ed7d2821cf0c7f06212f55438dda155bc75
Sean <zibai.wang@mail.utoronto.ca>
2014-01-24 19:09:29 -0500

    added ecea2

---
+++ ECEA2/src/com/example/ecea2/EnterNameActivity.java
@@ -0,0 +1,54 @@
+package com.example.ecea2;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class EnterNameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_enter_name);
+		Spinner s = (Spinner)findViewById(R.id.f_spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.food_spinner, android.R.layout.simple_spinner_dropdown_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		s.setAdapter(adapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.enter_name, menu);
+		return true;
+	}
+	
+	public void done(View view)
+	{
+		Intent intent = new Intent(this, MainActivity.class);
+		Bundle extras = new Bundle();
+		EditText editText1 = (EditText) findViewById(R.id.editfname);
+		String firstname = editText1.getText().toString();
+		EditText editText2 = (EditText) findViewById(R.id.editlname);
+		String lastname = editText2.getText().toString();
+		EditText editText3 = (EditText) findViewById(R.id.editage);
+		Spinner myspinner = (Spinner) findViewById(R.id.f_spinner);
+		String ffood = myspinner.getSelectedItem().toString();
+		String age = editText3.getText().toString();
+		extras.putString("EXTRA_FIRSTNAME",firstname);
+		extras.putString("EXTRA_LASTNAME",lastname);
+		extras.putString("EXTRA_AGE",age);
+		extras.putString("EXTRA_FFOOD",ffood);
+		intent.putExtras(extras);
+		startActivity(intent);
+		
+	}
+
+}
---
+++ ECEA2/src/com/example/ecea2/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.ecea2;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void exitapp(View view) {
+		finish();
+		System.exit(0);
+	}
+	
+	public void toname(View view) {
+		Intent intententer = new Intent(this, EnterNameActivity.class);
+		startActivity(intententer);
+	}
+	
+	public void store(View view){
+		Intent intent = getIntent();
+		Bundle extras = intent.getExtras();
+		String firstname = extras.getString("EXTRA_FIRSTNAME");
+		String lastname = extras.getString("EXTRA_LASTNAME");
+		String age = extras.getString("EXTRA_AGE");
+		String ffood = extras.getString("EXTRA_FFOOD");
+		Intent intent2 = new Intent(this, EnterFilenameActivity.class);
+		Bundle extras2 = new Bundle();
+		extras2.putString("EXTRA_FIRSTNAME",firstname);
+		extras2.putString("EXTRA_LASTNAME",lastname);
+		extras2.putString("EXTRA_AGE",age);
+		extras2.putString("EXTRA_FFOOD",ffood);
+		intent2.putExtras(extras2);
+        startActivity(intent2);
+	}
+	
+	public void load(View view) {
+		Intent intent = new Intent(this, LoadActivity.class);
+		startActivity(intent);
+	}
+	
+	public void view(View view) {
+		Intent intent = new Intent(this, AllFileActivity.class);
+		startActivity(intent);
+	}
+
+}
